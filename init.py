8bb15c221017bc64f2756486049bcb4a58067899
joao-victor-weber <133367093+joao-victor-weber@users.noreply.github.com>
2026-04-22 20:55:13 -0300

    Update init.py
    teste 2

--- init.py
+++ init.py
@@ -1,3 +1,14 @@
 import os
 import google.generativeia as genai
-genai.configure(api_key=chave_Api)
+genai.configure(api_key=(chave_Api))
+for ss in ganai.list_model():
+  if 'generateContent' in ss.supported_generation_methods:
+    model=genai.generativeModel('gemini-1.5-pro')
+chat=model.start_chat(history=[])
+context='responda em pt-br nativo'
+chat.send_message(context)
+conversa=chat.send_message(input('mande usas perguntas: '))
+while conversa != 'fechar':
+  resposta=chat.send_message(conversa)
+  print(resposta.text)
+  conversa=chat.send_message(input('Para sair digite "fechar", se não pergunte o que quiser: '))
